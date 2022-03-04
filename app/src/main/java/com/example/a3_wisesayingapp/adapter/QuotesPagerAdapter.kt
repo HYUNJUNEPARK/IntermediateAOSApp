169c1f8ed8e6c0c1cf42a0cc6fb0714a0e5c185b
박현준(Hyunjune Park) <parkhj@konai.com>
2022-03-04 10:13:49 +0900

    setPagerTransformet

--- app/src/main/java/com/example/a3_wisesayingapp/adapter/QuotesPagerAdapter.kt
+++ app/src/main/java/com/example/a3_wisesayingapp/adapter/QuotesPagerAdapter.kt
@@ -10,23 +10,25 @@ import com.example.a3_wisesayingapp.model.Quote
 class QuotesPagerAdapter(
     private val quotes: List<Quote>,
     private val isNameRevealed: Boolean
-): RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {
+): RecyclerView.Adapter<QuotesPagerAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemQuoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return QuoteViewHolder(binding)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        holder.bind(quotes[position], isNameRevealed)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val actualPositionIndex = position % quotes.size //quotes 포지션을 넘어가면 부터 시작해 뷰페이저를 반복할 수 있음
+        holder.bindData(quotes[actualPositionIndex], isNameRevealed)
     }
 
     override fun getItemCount(): Int {
-        return quotes.size
+        //return quotes.size 뷰페이저의 끝이 있음
+        return Int.MAX_VALUE //계속하다보면 페이지의 끝은 오지만 현실적으로 가능성이 매우 낮음
     }
 
-    inner class QuoteViewHolder(private val binding: ItemQuoteBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(quote:Quote, isNameRevealed: Boolean) {
+    inner class ViewHolder(private val binding: ItemQuoteBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindData(quote:Quote, isNameRevealed: Boolean) {
             binding.quoteTextView.text = "\"${quote.quote}\""
 
             if (isNameRevealed) {
