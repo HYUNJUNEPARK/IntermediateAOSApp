7e60c6a5f66c3b2c7a5fdeca860725f042b0578c
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-04-01 08:55:47 -0700

    arrange some codes

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 Firebase Authentication
--Email Login
+-Email Login / SignUp
+auth.signInWithEmailAndPassword(email, password)
+auth.createUserWithEmailAndPassword(email, password)
+
 -Facebook Login
+val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+auth.signInWithCredential(credential)
+credential : 보통 사용자 이름과 비밀번호의 조합이지만 보안 인증 툴에 의해 더 많은 요소가 추가될 수 있음
+
+
+
+
+
+///////////
+
 
 implementation 'com.google.firebase:firebase-auth-ktx' 추가
 ->Firebase class 사용 가능
--- app/src/main/java/com/example/tinder/activity/LoginActivity.kt
+++ app/src/main/java/com/example/tinder/activity/LoginActivity.kt
@@ -21,9 +21,8 @@ import com.google.firebase.ktx.Firebase
 class LoginActivity : AppCompatActivity() {
     private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     private lateinit var auth: FirebaseAuth
-    lateinit var emailEditText: EditText
-    lateinit var passwordEditText: EditText
-
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
     private lateinit var facebookCallbackManger: CallbackManager
 
     companion object {
@@ -40,34 +39,10 @@ class LoginActivity : AppCompatActivity() {
         emailEditText = binding.emailEditText
         passwordEditText = binding.passwordEditText
 
+        initButtonState()
         initLoginButton()
         initSingUpButton()
-        initEmailAndPasswordEditText()
         initFacebookLoginButton()
-
-    }
-
-    private fun initFacebookLoginButton() {
-        binding.facebookLoginButton.setPermissions("email", "public_profile") //이메일과 프로필 정보를 가져옴
-        binding.facebookLoginButton.registerCallback(facebookCallbackManger, object: FacebookCallback<LoginResult> {
-            override fun onSuccess(result: LoginResult) {
-                //로그인 액세스 토큰을 받으면 파이어베이스에 넘김
-                val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
-                auth.signInWithCredential(credential)
-                    .addOnCompleteListener(this@LoginActivity) { task ->
-                        if (task.isSuccessful) {
-                            finish()
-                        }
-                        else {
-                            Toast.makeText(this@LoginActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-            }
-            override fun onCancel() { }
-            override fun onError(error: FacebookException?) {
-                Toast.makeText(this@LoginActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
-            }
-        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -75,17 +50,18 @@ class LoginActivity : AppCompatActivity() {
         facebookCallbackManger.onActivityResult(requestCode, requestCode, data)
     }
 
-    private fun initEmailAndPasswordEditText() {
+    private fun initButtonState() {
         val emailTextView = emailEditText.text
         val passwordTextView = passwordEditText.text
 
         binding.emailEditText.addTextChangedListener {
-            val isEmpty = emailTextView.isNotEmpty() && passwordTextView.isNotEmpty()
+            val isEmpty:Boolean = emailTextView.isNotEmpty() && passwordTextView.isNotEmpty()
             binding.loginButton.isEnabled = isEmpty
             binding.signUpButton.isEnabled = isEmpty
         }
+
         binding.passwordEditText.addTextChangedListener {
-            val isEmpty = emailTextView.isNotEmpty() && passwordTextView.isNotEmpty()
+            val isEmpty:Boolean = emailTextView.isNotEmpty() && passwordTextView.isNotEmpty()
             binding.loginButton.isEnabled = isEmpty
             binding.signUpButton.isEnabled = isEmpty
         }
@@ -112,9 +88,9 @@ class LoginActivity : AppCompatActivity() {
         binding.signUpButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
+
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { signUpTask ->
-                    Log.d(TAG, "initSingUpButton: $signUpTask")
                     if (signUpTask.isSuccessful) {
                         Toast.makeText(this, getString(R.string.toast_signup_finish), Toast.LENGTH_SHORT).show()
                     }
@@ -123,5 +99,28 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
         }
+    }
+
+    private fun initFacebookLoginButton() {
+        binding.facebookLoginButton.setPermissions("email", "public_profile") //이메일, 프로필 정보를 가져옴(더 필요한 데이터가 있다면 페이스북 문서 참고)
+        binding.facebookLoginButton.registerCallback(facebookCallbackManger, object: FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult) {//로그인 액세스 토큰을 받아 credential 을 만들어 파이어베이스에 넘김
+                val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener(this@LoginActivity) { signInTask ->
+                        if (signInTask.isSuccessful) {
+                            finish()
+                        }
+                        else {
+                            Toast.makeText(this@LoginActivity, getString(R.string.toast_fail), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+            override fun onCancel() { }
+            override fun onError(error: FacebookException?) {
+                Log.e(TAG, "onError: $error")
+                Toast.makeText(this@LoginActivity, getString(R.string.toast_fail), Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
